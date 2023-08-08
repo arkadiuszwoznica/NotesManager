42a0aa939cde0845eafe496fd39a101bdddd5a60
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-08-08 12:49:45 +0200

    files for creating notes created

---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,3 @@
+export function NoteForm() {
+    return <h1>form</h1>
+}
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForm } from "../components/NoteForm"
+
+export function NewNote() {
+    return (
+        <>
+            <h1 className="mb-4">New note</h1>
+            <NoteForm/>
+        </>
+    )
+}
